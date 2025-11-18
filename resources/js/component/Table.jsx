3f284e8154a62c152639ef3ee43779ce711ec5e0
pooya <pooyajebreili@gmail.com>
2025-11-18 09:37:47 +0330

    ezafe kardan ghabeliat sort afrad bar asas name (A-Z & Z-A)

--- resources/js/component/Table.jsx
+++ resources/js/component/Table.jsx
@@ -1,14 +1,24 @@
 import React, { useState } from "react";
 import dataJson from "../data.json";
 import { FaTrash } from "react-icons/fa";
-
+import { FaSort } from "react-icons/fa";
 export default function Table() {
-
     const [users, setUsers] = useState(dataJson);
-
+    const [sortAsc, setSortAsc] = useState(true);
+    // --------------فانکشن حذف کاربر------------
     const deleteUser = (id) => {
-        const filtered = users.filter((item) => item.id !== id);
-        setUsers(filtered);
+        setUsers(users.filter((item) => item.id !== id));
+    };
+    // --------------فانکشنsortکاربر-------------
+    const sortByName = () => {
+        const sorted = [...users].sort((a, b) => {
+            return sortAsc
+                ? a.name.localeCompare(b.name) // A → Z
+                : b.name.localeCompare(a.name); // Z → A
+        });
+
+        setUsers(sorted);
+        setSortAsc(!sortAsc);
     };
 
     return (
@@ -18,18 +28,30 @@ export default function Table() {
                     <tr>
                         <th scope="col">Delete</th>
                         <th scope="col">id</th>
-                        <th scope="col">name</th>
+
+                        <th scope="col">
+                            <span
+                                className="bg-primary me-1 rounded-1 fs-5 p-1"
+                                style={{ cursor: "pointer" }}
+                                onClick={sortByName}
+                            >
+                                <FaSort />
+                            </span>
+                            name
+                        </th>
+
                         <th scope="col">email</th>
                         <th scope="col">role</th>
                         <th scope="col">status</th>
                         <th scope="col">date</th>
                     </tr>
                 </thead>
+
                 <tbody>
                     {users.map((item) => (
                         <tr key={item.id}>
                             <td>
-                                <span 
+                                <span
                                     className="text-danger"
                                     style={{ cursor: "pointer" }}
                                     onClick={() => deleteUser(item.id)}
